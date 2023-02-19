e442150c6cde425dfc1e63ac7329a8e6d387b668
Maximilian Huber <gh@maxhbr.de>
2023-02-19 14:32:02 +0100

    update
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- update.sh
+++ update.sh
@@ -2,4 +2,6 @@
 
 set -euo pipefail
 
- wget  --tries=2 -k -K  -E -r -l 10 -p -N -F --restrict-file-names=windows -nH localhost:3030
+wget  --tries=2 -k -K  -E -r -l 10 -p -N -F --restrict-file-names=windows -nH localhost:3030
+
+git add .
